e779f22c61dc0ef853f93c9c54297eb1eb5ecb46
trantrungtuan200304 <tran.trung.tuan.200304@gmail.com>
2025-02-11 22:10:29 +0700

    Update 007_removeDuplicates.cpp

--- 007_removeDuplicates.cpp
+++ 007_removeDuplicates.cpp
@@ -2,11 +2,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // Begin at the start of the array
         int left = 1;
         int right = 1;
         while (right < nums.size()) {
             if (nums[right] != nums[right - 1]) {
+                // if the number at position [right] does not equal with the number before [right - 1]
+                // then the number at the position [left] will be replaced with the number at the position [right]
                 nums[left] = nums[right];
+                // plus left to move on to the next number
                 left++;
                 right++;
             } else {
