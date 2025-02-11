208f3a04e1bbf99cc949795e44145a275c715dcc
trantrungtuan200304 <tran.trung.tuan.200304@gmail.com>
2025-02-11 21:59:38 +0700

    Create 007_removeDuplicates.cpp

---
+++ 007_removeDuplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left = 1;
+        int right = 1;
+        while (right < nums.size()) {
+            if (nums[right] != nums[right - 1]) {
+                nums[left] = nums[right];
+                left++;
+                right++;
+            } else {
+                right++;
+            }
+        }
+        return left;
+    }
+};
