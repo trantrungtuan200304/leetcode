0150e3798f96adb158380e22d4a349cbe3555498
trantrungtuan200304 <tran.trung.tuan.200304@gmail.com>
2025-02-12 21:36:32 +0700

    Create 009_moveZeroes.cpp

---
+++ 009_moveZeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // use two pointers
+        int left = 0;
+        int right = 0;
+        while (right < nums.size()) {
+            if (nums[right] != 0) {
+                swap(nums[left], nums[right]);
+                left++;
+                right++;
+            } else right++;
+        }
+    }
+};
